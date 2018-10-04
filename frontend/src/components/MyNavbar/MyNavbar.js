2d2623da8a934c6c2747dee8b8f21018a387f819
Oscar Holm <oscar_holm94@hotmail.com>
2018-10-04 16:55:43 +0200

    Added notification badge

--- frontend/src/components/MyNavbar/MyNavbar.js
+++ frontend/src/components/MyNavbar/MyNavbar.js
@@ -2,6 +2,7 @@ import React, { Component} from 'react'
 import { Navbar, Col } from 'react-materialize'
 import { NavLink, withRouter} from 'react-router-dom'
 import { connect } from 'react-redux'
+import NotificationBadge, { Effect } from 'react-notification-badge'
 // eslint-disable-next-line 
 import M from 'materialize-css'
 
@@ -49,32 +50,32 @@ class MyNavbar extends Component {
 		     alt='Logo'
 		     width='220px' />
 	    </Col>
-	return (
-	<Navbar brand={logo} right >
-	    <div>
-		<li className={this.getNavLinkClass('/')}><NavLink to='/'>
-		    <i className='material-icons icon-main left'>home</i>Startsida</NavLink></li>
-		<li className={this.getNavLinkClass('/info')}><NavLink to='/info'>
-		    <i className='material-icons icon-main left'>info</i>Info</NavLink></li>
-		<li className={this.getNavLinkClass('/priser')}><NavLink to='/priser'>
-		    <i className='material-icons icon-main left'>local_offer</i>Priser</NavLink></li>
-		<li className={this.getNavLinkClass('/kontakt')}><NavLink to='/kontakt'>
-		    <i className='material-icons icon-main left'>mail</i>Kontakt</NavLink></li>
-		<li className={this.getNavLinkClass('/fotografi')}><NavLink to='/fotografi'>
-		    <i className='material-icons icon-main left'>camera_alt</i>Fotografi</NavLink></li>
-		{ accessToken && <li className={this.getNavLinkClass('/notifikationer')}><NavLink to='/notifikationer'>
-		    <i className='material-icons icon-main left'>account_circle</i>Notifikationer</NavLink>
-		</li> }
-		    { accessToken && <li>{unread}</li>}
-		    { accessToken && <li className={this.getNavLinkClass('/logga_ut')}><NavLink to='/logga_ut'>
-			<i className='material-icons icon-main left'>keyboard_return</i>Logga ut</NavLink>
+	    return (
+	    <Navbar brand={logo} right >
+		<div>
+		    <li className={this.getNavLinkClass('/')}><NavLink to='/'>
+			<i className='material-icons icon-main left'>home</i>Startsida</NavLink></li>
+		    <li className={this.getNavLinkClass('/info')}><NavLink to='/info'>
+			<i className='material-icons icon-main left'>info</i>Info</NavLink></li>
+		    <li className={this.getNavLinkClass('/priser')}><NavLink to='/priser'>
+			<i className='material-icons icon-main left'>local_offer</i>Priser</NavLink></li>
+		    <li className={this.getNavLinkClass('/kontakt')}><NavLink to='/kontakt'>
+			<i className='material-icons icon-main left'>mail</i>Kontakt</NavLink></li>
+		    <li className={this.getNavLinkClass('/fotografi')}><NavLink to='/fotografi'>
+			<i className='material-icons icon-main left'>camera_alt</i>Fotografi</NavLink></li>
+		    { accessToken && <li className={this.getNavLinkClass('/notifikationer')}><NavLink to='/notifikationer'>
+			<i className='material-icons icon-main left'>account_circle</i>Notifikationer</NavLink>
 		    </li> }
-	    </div>
-	    <NavLink className="button-collapse" to='#' data-activates="nav-mobile">
-		<i className="material-icons">view_headline</i>
-	    </NavLink>
-	</Navbar>
-	)
+			{ accessToken && <li><NotificationBadge count={unread} effect={Effect.SCALE}/></li>}
+			{ accessToken && <li className={this.getNavLinkClass('/logga_ut')}><NavLink to='/logga_ut'>
+			    <i className='material-icons icon-main left'>keyboard_return</i>Logga ut</NavLink>
+			</li> }
+		</div>
+		<NavLink className="button-collapse" to='#' data-activates="nav-mobile">
+		    <i className="material-icons">view_headline</i>
+		</NavLink>
+	    </Navbar>
+	    )
     }
 }
 
